e0c1be92415c46d9215147f614e39e7b18865830
Miguel-AngelMartinez <miguel_1319104806@uptecamac.edu.mx>
2023-09-27 20:20:57 -0600

    Agregar archivos de proyecto.

---
+++ AutoLienzo(talleres)/AutoLienzo(talleres)/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AutoLienzo_talleres.View;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AutoLienzo_talleres_
+{
+    public partial class MainPage : ContentPage
+    {
+        public int segundero=0;
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeAsync();
+        }
+        private async void InitializeAsync()
+        {
+            await screenAsync();
+        }
+        public async Task screenAsync()
+        {
+            await Navigation.PushAsync(new welcome());
+        }
+       
+    }
+}
---
+++ AutoLienzo(talleres)/AutoLienzo(talleres)/View/welcome.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AutoLienzo_talleres.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class welcome : ContentPage
+    {
+        public welcome()
+        {
+            InitializeComponent();
+        }
+    }
+}
